7bc6006f1638502a016ff31cfc182632b317437c
Alex <zhogov.aa@phystech.edu>
2018-10-11 05:33:50 +0300

    tested div and css in .js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
+    },
+};
